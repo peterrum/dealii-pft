327511fa0a96bbf1a2329744667a3ebabfad3126
peterrum <peterrmuench@gmail.com>
2019-07-15 17:58:54 +0200

    Swap step 2 and 3

--- step-2/src/main.cpp
+++ step-2/src/main.cpp
@@ -11,128 +11,36 @@ const MPI_Comm comm = MPI_COMM_WORLD;
 
 using namespace dealii;
 
-template<int dim>
-void
-extract_info(const parallel::distributed::Triangulation<dim> & tria,
-             std::vector<CellData<dim>> &                      cells,
-             std::vector<Point<dim>> &                         vertices,
-             std::vector<int> &                                boundary_ids,
-             std::map<int, std::pair<int, int>> &              coarse_lid_to_gid,
-             std::vector<Part> &                               parts)
-{
-  // 1) enumerate cells of original triangulation globally uniquelly
-  FE_DGQ<dim>     fe(0);
-  DoFHandler<dim> dof_handler(tria);
-  dof_handler.distribute_dofs(fe);
-
-  // 2) collect vertices of active locally owned cells
-  std::set<unsigned int> vertices_owned_by_loclly_owned_cells;
-  for(auto cell : dof_handler.cell_iterators())
-    if(cell->active() && cell->is_locally_owned())
-      for(unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell; v++)
-        vertices_owned_by_loclly_owned_cells.insert(cell->vertex_index(v));
-
-  // helper function to determine if cell is locally relevant
-  auto is_ghost = [&](auto & cell) {
-    for(unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell; v++)
-      if(vertices_owned_by_loclly_owned_cells.find(cell->vertex_index(v)) !=
-         vertices_owned_by_loclly_owned_cells.end())
-        return true;
-    return false;
-  };
-
-  // 3) process all local and ghost cells: setup needed data structures and
-  //    collect all locally relevant vertices for second sweep
-  std::map<unsigned int, unsigned int> vertices_locally_relevant;
-  parts.push_back(Part());
-  Part & part = parts[0];
-
-  unsigned int cell_counter = 0;
-  for(auto cell : dof_handler.cell_iterators())
-    if(cell->active() && (cell->is_locally_owned() || is_ghost(cell)))
-    {
-      // a) extract cell definition (with old numbering of vertices)
-      CellData<dim> cell_data;
-      cell_data.material_id = cell->material_id();
-      for(unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell; v++)
-        cell_data.vertices[v] = cell->vertex_index(v);
-      cells.push_back(cell_data);
-
-      // b) save indices of each vertex of this cell
-      for(unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell; v++)
-        vertices_locally_relevant[cell->vertex_index(v)] = numbers::invalid_unsigned_int;
-
-      // c) save boundary_ids of each face of this cell
-      for(unsigned int f = 0; f < GeometryInfo<dim>::faces_per_cell; f++)
-        boundary_ids.push_back(cell->face(f)->boundary_id());
-
-      // d) determine gid of this cell
-      std::vector<types::global_dof_index> indices(1);
-      cell->get_dof_indices(indices);
-
-      // e) save translation for corase grid: lid -> gid
-      coarse_lid_to_gid[cell_counter] = {indices[0], cell->subdomain_id()};
-
-      // f) save level information
-      if(cell->is_locally_owned())
-      {
-        // gid of local cells
-        part.local.push_back(indices[0]);
-      }
-      else
-      {
-        // gid, subdomain_id, and leve_subdomain_id of ghost cells
-        part.ghost.push_back(indices[0]);
-        part.ghost_rank.push_back(cell->subdomain_id());
-        part.ghost_rank_mg.push_back(cell->level_subdomain_id());
-      }
-      cell_counter++;
-    }
-
-  // 4) enumerate locally relevant
-  unsigned int vertex_counter = 0;
-  for(auto & vertex : vertices_locally_relevant)
-  {
-    vertices.push_back(tria.get_vertices()[vertex.first]);
-    vertex.second = vertex_counter++;
-  }
-
-  // 5) correct vertices of cells (make them local)
-  for(auto & cell : cells)
-    for(unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell; v++)
-      cell.vertices[v] = vertices_locally_relevant[cell.vertices[v]];
-}
-
-
 
 template<int dim>
 void
-test(int n_refinements, MPI_Comm comm)
+test(const int n_refinements, const int n_subdivisions, MPI_Comm comm)
 {
-  // create pdt
-  parallel::distributed::Triangulation<dim> tria_pdt(
-    comm,
-    dealii::Triangulation<dim>::none,
-    parallel::distributed::Triangulation<dim>::construct_multigrid_hierarchy);
-  GridGenerator::hyper_cube(tria_pdt);
-  tria_pdt.refine_global(n_refinements);
-
-  // extract relevant information from pdt to be able to create pft
-  std::vector<CellData<dim>>         cells;
-  std::vector<Point<dim>>            vertices;
-  std::vector<int>                   boundary_ids;
-  std::map<int, std::pair<int, int>> coarse_lid_to_gid;
-  std::vector<Part>                  parts;
-
-  extract_info(tria_pdt, cells, vertices, boundary_ids, coarse_lid_to_gid, parts);
-
   // create pft
   parallel::fullydistributed::Triangulation<dim> tria_pft(comm);
-  tria_pft.reinit(cells, vertices, boundary_ids, coarse_lid_to_gid, parts);
 
-  // output meshes as VTU
+  tria_pft.reinit(n_refinements, [&](dealii::Triangulation<dim> & tria) mutable {
+    GridGenerator::subdivided_hyper_cube(tria, n_subdivisions);
+    tria.refine_global(n_refinements);
+    
+      for (auto &cell : tria.active_cell_iterators())
+        {
+          for (unsigned int v = 0; v < GeometryInfo<dim>::vertices_per_cell; ++v)
+            {
+              if (cell->vertex(v)[0] < 0.5)
+                {
+                  cell->set_refine_flag();
+                  break;
+                }
+            }
+        }
+      tria.execute_coarsening_and_refinement();
+    
+  });
+
+
+  // output mesh as VTU
   GridOut grid_out;
-  grid_out.write_mesh_per_processor_as_vtu(tria_pdt, "trid_pdt", true, true);
   grid_out.write_mesh_per_processor_as_vtu(tria_pft, "trid_pft", true, true);
 }
 
@@ -143,15 +51,18 @@ main(int argc, char ** argv)
 {
   Utilities::MPI::MPI_InitFinalize mpi(argc, argv, 1);
 
-  AssertThrow(argc > 2, ExcMessage("You have not provided two command-line arguments."));
+  AssertThrow(argc > 3, ExcMessage("You have not provided two command-line arguments."));
 
-  const int dim           = atoi(argv[1]);
-  const int n_refinements = atoi(argv[2]);
+  const int dim            = atoi(argv[1]);
+  const int n_refinements  = atoi(argv[2]);
+  const int n_subdivisions = atoi(argv[3]);
 
-  if(dim == 2)
-    test<2>(n_refinements, comm);
+  if(dim == 1)
+    test<2>(n_refinements, n_subdivisions, comm);
+  else if(dim == 2)
+    test<2>(n_refinements, n_subdivisions, comm);
   else if(dim == 3)
-    test<3>(n_refinements, comm);
+    test<3>(n_refinements, n_subdivisions, comm);
   else
-    AssertThrow(false, ExcMessage("Only working for dimensions 2 and 3!"));
+    AssertThrow(false, ExcMessage("Only working for dimensions 1, 2, and 3!"));
 }
