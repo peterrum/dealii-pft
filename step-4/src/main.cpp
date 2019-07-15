2a07e86144eb33cdeb2bd3788de4546e71881149
peterrum <peterrmuench@gmail.com>
2019-07-15 11:06:11 +0200

    Add step-4

---
+++ step-4/src/main.cpp
@@ -0,0 +1,52 @@
+#include <deal.II/base/geometry_info.h>
+#include <deal.II/base/mpi.h>
+#include <deal.II/distributed/tria.h>
+#include <deal.II/dofs/dof_accessor.h>
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/fe/fe_dgq.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+
+const MPI_Comm comm = MPI_COMM_WORLD;
+
+using namespace dealii;
+
+
+template<int dim>
+void
+test(const int n_refinements, const int n_subdivisions, MPI_Comm comm)
+{
+  // create pft
+  parallel::fullydistributed::Triangulation<dim> tria_pft(comm);
+  
+  tria_pft.reinit(n_refinements, [&](dealii::Triangulation<dim> & tria) mutable {
+          GridGenerator::subdivided_hyper_cube(tria, n_subdivisions);
+          tria.refine_global(n_refinements);
+        });
+
+
+  // output mesh as VTU
+  GridOut grid_out;
+  grid_out.write_mesh_per_processor_as_vtu(tria_pft, "trid_pft", true, true);
+}
+
+
+
+int
+main(int argc, char ** argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi(argc, argv, 1);
+
+  AssertThrow(argc > 3, ExcMessage("You have not provided two command-line arguments."));
+
+  const int dim            = atoi(argv[1]);
+  const int n_refinements  = atoi(argv[2]);
+  const int n_subdivisions = atoi(argv[3]);
+
+  if(dim == 2)
+    test<2>(n_refinements, n_subdivisions, comm);
+  else if(dim == 3)
+    test<3>(n_refinements, n_subdivisions, comm);
+  else
+    AssertThrow(false, ExcMessage("Only working for dimensions 2 and 3!"));
+}
