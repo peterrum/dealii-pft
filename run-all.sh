514f37134b7b1d0c893f0edf1b2fccc6e6f0f744
peterrum <peterrmuench@gmail.com>
2019-07-19 11:04:00 +0200

    Add new step for hanging nodes

--- run-all.sh
+++ run-all.sh
@@ -1,34 +1,47 @@
 make -j10
 
+# fine grid of serial triangulation
 cd step-0
 mpirun -np 5 ./step-0 2 4
 cd ..
 
 
+
+# fine grid of distributed triangulation
 cd step-1
 mpirun -np 5 ./step-1 2 8
 cd ..
 
 
 
+# multigrid levels of distributed triangulation (+hanging node)
+cd step-2 
+mpirun -np 7 ./step-2 2 4 10
+cd ..
+
+
+
+# multigrid levels of serial triangulation
 cd step-3 
 mpirun -np 5 ./step-3 2 4 8
 cd ..
 
 
 
+# multigrid levels of distributed triangulation
 cd step-4
 mpirun -np 5 ./step-4 2 4 8
 cd ..
 
 
+# partitioning of serial mesh 
+#cd step-5
+#mpirun -np 5 ./step-5 2 4 8
+#cd ..
 
-cd step-5
-mpirun -np 5 ./step-5 2 4 8
-cd ..
 
 
-
-cd step-6 
-mpirun -np 5 ./step-6 2 4 8
-cd ..
+# partitioning of serial mesh with periodic bcs
+#cd step-6 
+#mpirun -np 5 ./step-6 2 4 8
+#cd ..
---
+++ step-2/src/main.cpp
@@ -0,0 +1,90 @@
+#include <deal.II/base/mpi.h>
+#include <deal.II/distributed/tria.h>
+#include <deal.II/fe/fe_q.h>
+#include <deal.II/grid/grid_generator.h>
+#include <deal.II/grid/grid_out.h>
+
+#include "../../common/utilities.h"
+
+const MPI_Comm comm = MPI_COMM_WORLD;
+
+using namespace dealii;
+
+
+
+template<int dim>
+void
+test(int n_refinements, const int n_subdivisions, MPI_Comm comm)
+{
+  // create pdt
+  parallel::distributed::Triangulation<dim> tria_pdt(
+    comm,
+    dealii::Triangulation<dim>::none,
+    parallel::distributed::Triangulation<dim>::construct_multigrid_hierarchy);
+
+
+  const Point<dim> center(1, 0);
+  const double     inner_radius = 0.5, outer_radius = 1.0;
+  GridGenerator::hyper_shell(tria_pdt, center, inner_radius, outer_radius, n_subdivisions);
+  // tria_pdt.reset_all_manifolds ();
+  for(int step = 0; step < n_refinements; ++step)
+  {
+    for(auto & cell : tria_pdt.active_cell_iterators())
+    {
+      // if(cell->is_locally_owned ())
+      for(unsigned int v = 0; v < GeometryInfo<2>::vertices_per_cell; ++v)
+      {
+        const double distance_from_center = center.distance(cell->vertex(v));
+        if(std::fabs(distance_from_center - inner_radius) < 1e-10)
+        {
+          cell->set_refine_flag();
+          break;
+        }
+      }
+    }
+    tria_pdt.execute_coarsening_and_refinement();
+  }
+
+  // create instance of pft
+  parallel::fullydistributed::Triangulation<dim> tria_pft(
+    comm, parallel::fullydistributed::Triangulation<dim>::construct_multigrid_hierarchy);
+
+  // extract relevant information form pdt
+  auto construction_data =
+    parallel::fullydistributed::Utilities::copy_from_triangulation(tria_pdt, tria_pft);
+
+  // actually create triangulation
+  tria_pft.reinit(construction_data);
+
+  // test triangulation
+  FE_Q<dim>       fe(2);
+  DoFHandler<dim> dof_handler(tria_pft);
+  dof_handler.distribute_dofs(fe);
+  dof_handler.distribute_mg_dofs();
+
+  // output meshes as VTU
+  GridOut grid_out;
+  grid_out.write_mesh_per_processor_as_vtu(tria_pdt, "trid_pdt", true, true);
+  grid_out.write_mesh_per_processor_as_vtu(tria_pft, "trid_pft", true, true);
+}
+
+
+
+int
+main(int argc, char ** argv)
+{
+  Utilities::MPI::MPI_InitFinalize mpi(argc, argv, 1);
+
+  AssertThrow(argc > 3, ExcMessage("You have not provided two command-line arguments."));
+
+  const int dim            = atoi(argv[1]);
+  const int n_refinements  = atoi(argv[2]);
+  const int n_subdivisions = atoi(argv[3]);
+
+  if(dim == 2)
+    test<2>(n_refinements, n_subdivisions, comm);
+  else if(dim == 3)
+    test<3>(n_refinements, n_subdivisions, comm);
+  else
+    AssertThrow(false, ExcMessage("Only working for dimensions 2 and 3!"));
+}
